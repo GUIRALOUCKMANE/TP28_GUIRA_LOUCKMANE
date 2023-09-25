a1d70e4ec19c423f838a1fcd2b17d30c8d4d1633
GUIRA LOUCKMANE <guiralouckmane508@gmail.com>
2023-09-25 15:28:50 +0000

    TP28GUIRA_LOUCKMANE

---
+++ menu.php
@@ -0,0 +1,20 @@
+
+ 
+    <nav>
+        <a href="index.html"><img src="armoiries.png" alt="" class="lou"></a>
+        <div class="nav-links" id="nav-links">
+        <i class=" fa fa-times"onclick="hideMenu()"></i>
+        
+            <ul>
+            
+                <li> <a href="index.php">ACCUEIL</a></li>
+                <li> <a href="service.php">SERVICE</a></li>
+                
+                <li> <a href="about.php">about</a></li>
+                <li> <a href="contact.php">CONTACT</a></li>
+                
+            
+            </ul>
+        </di>
+        <i class="fa fa-bars" onclick="showMEnu"></i>
+        </nav>
